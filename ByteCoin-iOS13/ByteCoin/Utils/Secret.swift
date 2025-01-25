9d83da91ba8903630b4a6350d01ba69cdf3b143c
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-01-25 14:15:20 +0700

    section 14 delete api key

--- ByteCoin-iOS13/ByteCoin/Utils/Secret.swift
+++ ByteCoin-iOS13/ByteCoin/Utils/Secret.swift
@@ -7,6 +7,6 @@
 //
 
 
-//    let exchangeRateAPIKey = "57a8f839-d287-4de6-8ae5-27aa3f33ffec"
+//    let exchangeRateAPIKey = "#"
 
-let marketRateAPIKey = "6e9760e3-e5df-4aac-92e1-ad844bbcc40b"
+let marketRateAPIKey = "#"
